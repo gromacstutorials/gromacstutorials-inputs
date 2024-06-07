c413993e546ab467045f81cb27d42522a7ed0db8
Simon Gravelle <simon.gravelle@live.fr>
2024-06-07 10:17:12 +0200

    tried exercice

--- level1/bulk-solution/exercises/cage-effect/localRun.sh
+++ level1/bulk-solution/exercises/cage-effect/localRun.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-#gmx=/home/simon/Softwares/gromacs-2024.2/build/bin/gmx
-gmx=gmx
+gmx=/home/simon/Softwares/gromacs-2024.2/build/bin/gmx
 
 ${gmx} grompp -f inputs/production.mdp -c npt.gro -p topol.top -o production -pp production -po production
 ${gmx} mdrun -v -deffnm production -nt 8
+
+${gmx} convert-trj -f production.xtc -s production.tpr -o production.xtc -dt 1
 
 ${gmx} msd -f production.xtc -s production.tpr -o na-msd.xvg << EOF
     3
